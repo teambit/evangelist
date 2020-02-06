08abb468350edd3733b7123812bb6b600a9ab171
oded <oded@bit.dev>
2020-02-06 14:58:44 +0200

    fixed carousel bug and updated fold6

--- src/folds/fold2/fold2.tsx
+++ src/folds/fold2/fold2.tsx
@@ -1,7 +1,8 @@
 import React, { HTMLAttributes } from 'react';
 import classNames from 'classnames';
 
-
+//@ts-ignore
+import {CommentCarousel, carouselContent} from "@bit/bit.web.components.comment-carousel";
 import { PossibleSizes } from '../../constants/sizes';
 import { H1, H2, H5 } from '../../components/concrete/heading';
 import styles from './fold2.module.scss';
@@ -10,7 +11,7 @@ export function Fold2(props: HTMLAttributes<HTMLDivElement>) {
 	return (
 		<div {...props} className={classNames(styles.fold2, props.className)}>
 		<H2 size={PossibleSizes.md}>Trusted by over 100,000 developers</H2>
-		{/* <CommentCarousel content={carouselContent} /> */}
+		<CommentCarousel content={carouselContent} />
 	</div>
 	);
 }
--- src/folds/fold6/fold6.tsx
+++ src/folds/fold6/fold6.tsx
@@ -9,19 +9,19 @@ import { Label } from '../../components/base/label';
 import styles from './fold6.module.scss';
 
 export function Fold6(props: HTMLAttributes<HTMLDivElement>) {
-	
 	return (
 		<div {...props} className={classNames(styles.fold6, props.className)}>
 			<Label className={styles.ctaLabel}>Develop</Label>
-			<div>WIP + RIP</div>
-			<H2 size={PossibleSizes.md}>Develop components in simple decoupled codebases</H2>
+			<H2 size={PossibleSizes.md}>
+				Incremental deployments with first class static hosting
+			</H2>
 			<div className={styles.splitCard}>
 				<div>
-					<H2 size={PossibleSizes.xs}>Simple yet scalable semantic versioning.</H2>
+					<H2 size={PossibleSizes.xs}>Continuously deploy components.</H2>
 					<Paragraph>
-						Leverage semantic versioning to control the development of each
-						component. Deploy, update, or rollback specific components across
-						different screens, features and apps.
+						Leverage per-component CD to independently deploy components to
+						production. Empower teams to consistently deliver new features and
+						upgrades, end-to-end.
 					</Paragraph>
 				</div>
 				<img src="https://static.bit.dev/homepage-bit/2-simple.png" />
@@ -29,12 +29,13 @@ export function Fold6(props: HTMLAttributes<HTMLDivElement>) {
 			<div className={styles.splitCard}>
 				<img src="https://static.bit.dev/homepage-bit/2-per.png" />
 				<div>
-					<H2 size={PossibleSizes.xs}>Per component development environment.</H2>
+					<H2 size={PossibleSizes.xs}>
+						Constantly deliver upgrades, hotfixes and rollbacks.
+					</H2>
 					<Paragraph>
-						When writing a component, Bit spawns an development environment that
-						lets you build, test and render modular components in isolation. It
-						works with your favorite tools, and, it uses pre-made reusable
-						configurations to save you time. Best practices included.
+						Empower frontend teams to deliver and innovate in short effective
+						cycles. Give each team the power to dynamically upgrade, hotfix or
+						rollback any part of your UI on its own.
 					</Paragraph>
 				</div>
 			</div>
