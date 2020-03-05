917c97c569362c03997ccdd5d1594651e6e394ec
Uri Kutner <urikutner@gmail.com>
2020-03-05 11:45:00 +0200

    left align comment carousel

--- src/sections/carouselSection/carouselSection.tsx
+++ src/sections/carouselSection/carouselSection.tsx
@@ -1,5 +1,4 @@
 import React, { HTMLAttributes } from 'react';
-import classNames from 'classnames';
 
 //@ts-ignore
 import { CommentCarousel, carouselContent } from '@bit/bit.web.components.comment-carousel';
@@ -9,7 +8,7 @@ import styles from './carouselSection.module.scss';
 
 export function CarouselSection(props: HTMLAttributes<HTMLDivElement>) {
 	return (
-		<div {...props} className={classNames(styles.carouselSection, props.className)}>
+		<div {...props} className={props.className}>
 			<H2 size={PossibleSizes.sm} className={styles.heading}>
 				Trusted by over 100,000 developers
 			</H2>
