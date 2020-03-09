70a11625a4cd9078194471fc1c9d8017d2a4af88
Ran Mizrahi <ran@bit.dev>
2020-03-10 01:16:33 +0200

    text update

--- src/sections/booking-breakdown/booking-breakdown.tsx
+++ src/sections/booking-breakdown/booking-breakdown.tsx
@@ -16,7 +16,7 @@ export function BookingBreakdown(props: HTMLAttributes<HTMLDivElement>) {
 		<div {...props} data-bit-id="sections/booking-breakdown">
 			<div className={classNames(textColumn, marginCenter)}>
 				<H2 size={PossibleSizes.sm}>
-					Enjoy the best tools to build and distribute components.
+					Everything you need to build independent and reusable components
 				</H2>
 				<Paragraph size={PossibleSizes.lg} className={classNames(styles.subtitle)}>
 					Bit provides the most powerful tools and experience to develop, build, test
