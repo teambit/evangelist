45be58ee0d8bbae3d73443ec49ea72b28dd74d54
Uri Kutner <urikutner@gmail.com>
2020-03-17 18:38:43 +0200

    add scope to components display name

--- src/components/concrete/components-highlight-dictionary/components-dictionary.ts
+++ src/components/concrete/components-highlight-dictionary/components-dictionary.ts
@@ -4,160 +4,160 @@ const scopeUrl = 'https://bit.dev/bit/marketing';
 
 export const componentDictionary: ComponentHighlightDictionary = {
 	'base/paragraph': {
-		displayName: 'base/paragraph',
+		displayName: 'bit.marketing/base/paragraph',
 		url: `${scopeUrl}/base/paragraph`,
 	},
 	'base/primary-button': {
-		displayName: 'base/primary-button',
+		displayName: 'bit.marketing/base/primary-button',
 		url: `${scopeUrl}/base/primary-button`,
 	},
 	'base/label': {
-		displayName: 'base/label',
+		displayName: 'bit.marketing/base/label',
 		url: `${scopeUrl}/base/label`,
 	},
 	'base/separator': {
-		displayName: 'base/separator',
+		displayName: 'bit.marketing/base/separator',
 		url: `${scopeUrl}/base/separator`,
 	},
 	'base/link': {
-		displayName: 'base/link',
+		displayName: 'bit.marketing/base/link',
 		url: `${scopeUrl}/base/link`,
 	},
 	'base/themed-text': {
-		displayName: 'base/themed-text',
+		displayName: 'bit.marketing/base/themed-text',
 		url: `${scopeUrl}/base/themed-text`,
 	},
 	'base/image': {
-		displayName: 'base/image',
+		displayName: 'bit.marketing/base/image',
 		url: `${scopeUrl}/base/image`,
 	},
 
 	'base/heading': {
-		displayName: 'base/heading',
+		displayName: 'bit.marketing/base/heading',
 		url: `${scopeUrl}/base/heading`,
 	},
 	'concrete/heading': {
-		displayName: 'concrete/heading',
+		displayName: 'bit.marketing/concrete/heading',
 		url: `${scopeUrl}/concrete/heading`,
 	},
 	'concrete/image': {
-		displayName: 'concrete/image',
+		displayName: 'bit.marketing/concrete/image',
 		url: `${scopeUrl}/concrete/image`,
 	},
 	'concrete/cta-button': {
-		displayName: 'concrete/cta-button',
+		displayName: 'bit.marketing/concrete/cta-button',
 		url: `${scopeUrl}/concrete/cta-button`,
 	},
 	'concrete/icon-line': {
-		displayName: 'concrete/icon-line',
+		displayName: 'bit.marketing/concrete/icon-line',
 		url: `${scopeUrl}/concrete/icon-line`,
 	},
 	'concrete/marketing-card': {
-		displayName: 'concrete/marketing-card',
+		displayName: 'bit.marketing/concrete/marketing-card',
 		url: `${scopeUrl}/concrete/marketing-card`,
 	},
 	'concrete/twitter-card': {
-		displayName: 'concrete/twitter-card',
+		displayName: 'bit.marketing/concrete/twitter-card',
 		url: `${scopeUrl}/concrete/twitter-card`,
 	},
 	'concrete/comment-carousel': {
-		displayName: 'concrete/comment-carousel',
+		displayName: 'bit.marketing/concrete/comment-carousel',
 		url: `${scopeUrl}/concrete/comment-carousel`,
 	},
 
 	'community-links/slack-link': {
-		displayName: 'community-links/slack-link',
+		displayName: 'bit.marketing/community-links/slack-link',
 		url: `${scopeUrl}/community-links/slack-link`,
 	},
 	'community-links/twitter-link': {
-		displayName: 'community-links/twitter-link',
+		displayName: 'bit.marketing/community-links/twitter-link',
 		url: `${scopeUrl}/community-links/twitter-link`,
 	},
 	'community-links/github-link': {
-		displayName: 'community-links/github-link',
+		displayName: 'bit.marketing/community-links/github-link',
 		url: `${scopeUrl}/community-links/github-link`,
 	},
 
 	'sections/main-section': {
-		displayName: 'sections/main-section',
+		displayName: 'bit.marketing/sections/main-section',
 		url: `${scopeUrl}/sections/main-section`,
 	},
 	'sections/summary-cards': {
-		displayName: 'sections/summary-cards',
+		displayName: 'bit.marketing/sections/summary-cards',
 		url: `${scopeUrl}/sections/summary-cards`,
 	},
 	'sections/dev-section': {
-		displayName: 'sections/dev-section',
+		displayName: 'bit.marketing/sections/dev-section',
 		url: `${scopeUrl}/sections/dev-section`,
 	},
 	'sections/auto-section': {
-		displayName: 'sections/auto-section',
+		displayName: 'bit.marketing/sections/auto-section',
 		url: `${scopeUrl}/sections/auto-section`,
 	},
 	'sections/cta': {
-		displayName: 'sections/cta',
+		displayName: 'bit.marketing/sections/cta',
 		url: `${scopeUrl}/sections/cta`,
 	},
 	'sections/build-section': {
-		displayName: 'sections/build-section',
+		displayName: 'bit.marketing/sections/build-section',
 		url: `${scopeUrl}/sections/build-section`,
 	},
 	'sections/deploy-section': {
-		displayName: 'sections/deploy-section',
+		displayName: 'bit.marketing/sections/deploy-section',
 		url: `${scopeUrl}/sections/deploy-section`,
 	},
 	'sections/reuse': {
-		displayName: 'sections/reuse',
+		displayName: 'bit.marketing/sections/reuse',
 		url: `${scopeUrl}/sections/reuse`,
 	},
 	'sections/community': {
-		displayName: 'sections/community',
+		displayName: 'bit.marketing/sections/community',
 		url: `${scopeUrl}/sections/community`,
 	},
 	'sections/auto-docs': {
-		displayName: 'sections/auto-docs',
+		displayName: 'bit.marketing/sections/auto-docs',
 		url: `${scopeUrl}/sections/auto-docs`,
 	},
 	'sections/booking-breakdown': {
-		displayName: 'sections/booking-breakdown',
+		displayName: 'bit.marketing/sections/booking-breakdown',
 		url: `${scopeUrl}/sections/booking-breakdown`,
 	},
 	'sections/component-advantages': {
-		displayName: 'sections/component-advantages',
+		displayName: 'bit.marketing/sections/component-advantages',
 		url: `${scopeUrl}/sections/component-advantages`,
 	},
 	'sections/enterprise-section': {
-		displayName: 'sections/enterprise-section',
+		displayName: 'bit.marketing/sections/enterprise-section',
 		url: `${scopeUrl}/sections/enterprise-section`,
 	},
 	'sections/hybrid-perfect-sync': {
-		displayName: 'sections/hybrid-perfect-sync',
+		displayName: 'bit.marketing/sections/hybrid-perfect-sync',
 		url: `${scopeUrl}/sections/hybrid-perfect-sync`,
 	},
 	'sections/independent-teams': {
-		displayName: 'sections/independent-teams',
+		displayName: 'bit.marketing/sections/independent-teams',
 		url: `${scopeUrl}/sections/independent-teams`,
 	},
 	'sections/reuse-landing-section': {
-		displayName: 'sections/reuse-landing-section',
+		displayName: 'bit.marketing/sections/reuse-landing-section',
 		url: `${scopeUrl}/sections/reuse-landing-section`,
 	},
 	'sections/sync-section': {
-		displayName: 'sections/sync-section',
+		displayName: 'bit.marketing/sections/sync-section',
 		url: `${scopeUrl}/sections/sync-section`,
 	},
 	'sections/talk-to-experts': {
-		displayName: 'sections/talk-to-experts',
+		displayName: 'bit.marketing/sections/talk-to-experts',
 		url: `${scopeUrl}/sections/talk-to-experts`,
 	},
 
 	'layout/grid': {
-		displayName: 'layout/grid',
+		displayName: 'bit.marketing/layout/grid',
 		url: `${scopeUrl}/layout/grid`,
 	},
 
 	commentCarousel: {
-		displayName: 'commentCarousel',
+		displayName: 'bit.marketing/commentCarousel',
 		url: 'https://bit.dev/bit/web/components/comment-carousel',
 	},
 };
