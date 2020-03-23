c248ea8a866f541052574f60646fb10f767c696f
Uri Kutner <urikutner@gmail.com>
2020-03-23 21:18:21 +0200

    readme and licence

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { Theme } from './themes/theme-provider';
 
 import { EmbedIconSet } from './components/concrete/icon';
 
-// import { Homepage } from './pages/homepage';
-// import { ReusePage } from './pages/reuse';
 import { HybridHomepage } from './pages/hybrid-homepage';
 import { ComponentHighlighter } from './components/concrete/component-highlighter';
 import { componentDictionary } from './content/components-highlight-dictionary';
@@ -16,7 +14,11 @@ class App extends PureComponent {
 	render() {
 		return (
 			<Theme>
-				<ComponentHighlighter active componentsDictionary={componentDictionary}>
+				<ComponentHighlighter
+					active
+					componentsDictionary={componentDictionary}
+					versionMap={{ 'bit.evangelist/sections/reuse-landing-section': '1.0.5' }}
+				>
 					<EmbedIconSet query="aqq93z" />
 					<HybridHomepage githubStars={10000} />
 				</ComponentHighlighter>
