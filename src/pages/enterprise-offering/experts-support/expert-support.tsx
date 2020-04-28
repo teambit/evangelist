be44609d78cb53aadccd9430928fd1cc695acf8d
Uri Kutner <urikutner@gmail.com>
2020-04-28 18:08:36 +0300

    responsivesss + chat heads

--- src/pages/enterprise-offering/experts-support/expert-support.tsx
+++ src/pages/enterprise-offering/experts-support/expert-support.tsx
@@ -1,102 +1,81 @@
 import React, { HTMLAttributes } from 'react';
+import classNames from 'classnames';
 
 import { Card } from '@bit/bit.base-ui.surfaces.card';
 import { Paragraph } from '@bit/bit.base-ui.text.paragraph';
 import { PossibleSizes } from '@bit/bit.base-ui.theme.sizes';
-import { MutedText } from '@bit/bit.base-ui.text.muted-text';
-import { text } from '@bit/bit.base-ui.layout.align';
+import { MutedText, mutedText } from '@bit/bit.base-ui.text.muted-text';
+import { text, marginCenter } from '@bit/bit.base-ui.layout.align';
 
 import { H2, H4 } from '../../../elements/heading';
 import { Image } from '../../../elements/image';
+import { margin } from '../spacing';
+
+import styles from './expert-support.module.scss';
 
 export const ExpertSupport = (props: HTMLAttributes<HTMLDivElement>) => (
-	<div {...props}>
-		<div style={{ maxWidth: '490px', margin: 'auto' }}>
-			<H2
-				size={PossibleSizes.md}
-				style={{ marginBottom: '60px' }}
-				className={text.center}
-			>
-				Enjoy world-class support and hands-on experts
-			</H2>
+	<div className={styles.preventOverflow}>
+		<div {...props} className={classNames(styles.container, props.className)}>
+			<div className={classNames(styles.mainContent, marginCenter)}>
+				<H2 size={PossibleSizes.md} className={classNames(text.center, margin[50])}>
+					Enjoy world-class support and hands-on experts
+				</H2>
 
-			<Card
-				style={{
-					background: 'white',
-					marginBottom: '18px',
-					padding: '24px',
-					borderRadius: '20px',
-				}}
-			>
-				<div style={{ marginBottom: '7px' }}>
-					<MutedText>10:30</MutedText>
-				</div>
-				<H4 size={PossibleSizes.xxs}>Response-time SLA</H4>
-				<Paragraph>
-					<MutedText>
+				<Card className={styles.card}>
+					<MutedText className={styles.timestamp}>10:30</MutedText>
+					<H4 size={PossibleSizes.xxs}>Response-time SLA</H4>
+					<Paragraph className={mutedText}>
 						Enjoy industry-leading response times and availability through multiple
 						support channels including named contacts, joint Slack and more. Never
 						slow down.
-					</MutedText>
-				</Paragraph>
-			</Card>
-			<Card
-				style={{
-					background: 'white',
-					marginBottom: '18px',
-					padding: '24px',
-					borderRadius: '20px',
-				}}
-			>
-				<div style={{ marginBottom: '7px' }}>
-					<MutedText>10:31</MutedText>
-				</div>
-				<H4 size={PossibleSizes.xxs}>Named contacts with escalation paths</H4>
-				<MutedText>
-					Get named Bit experts with your success as their personal goal, and enjoy
-					multiple escalation paths vis email, chat and phone.
-				</MutedText>
-			</Card>
-			<Card
-				style={{
-					background: 'white',
-					marginBottom: '18px',
-					padding: '24px',
-					borderRadius: '20px',
-				}}
-			>
-				<div style={{ marginBottom: '7px' }}>
-					<MutedText>10:32</MutedText>
-				</div>
-				<H4 size={PossibleSizes.xxs}>Expert review and training</H4>
-				<MutedText>
-					Get architectural-level consultation from industry-leading experts and enjoy
-					hands-on team training and onboarding with best practices and personalized
-					workflows.
-				</MutedText>
-			</Card>
-		</div>
+					</Paragraph>
+				</Card>
+				<Card className={styles.card}>
+					<MutedText className={styles.timestamp}>10:31</MutedText>
+					<H4 size={PossibleSizes.xxs}>Named contacts with escalation paths</H4>
+					<Paragraph className={mutedText}>
+						Get named Bit experts with your success as their personal goal, and
+						enjoy multiple escalation paths vis email, chat and phone.
+					</Paragraph>
+				</Card>
+				<Card className={styles.card}>
+					<MutedText className={styles.timestamp}>10:32</MutedText>
+					<H4 size={PossibleSizes.xxs}>Expert review and training</H4>
+					<Paragraph className={mutedText}>
+						Get architectural-level consultation from industry-leading experts and
+						enjoy hands-on team training and onboarding with best practices and
+						personalized workflows.
+					</Paragraph>
+				</Card>
+			</div>
 
-		<Image src="enterprise-offering-v1/7-support/characters/character-1.svg" alt="yo yo" />
-		<Image
-			src="enterprise-offering-v1/7-support/characters/character-2.svg"
-			alt="how about slack?"
-		/>
-		<Image
-			src="enterprise-offering-v1/7-support/characters/character-3.svg"
-			alt="typing.."
-		/>
-		<Image
-			src="enterprise-offering-v1/7-support/characters/character-4.svg"
-			alt="floating head"
-		/>
-		<Image
-			src="enterprise-offering-v1/7-support/characters/character-5.svg"
-			alt="floating head"
-		/>
-		<Image
-			src="enterprise-offering-v1/7-support/characters/character-6.svg"
-			alt="we can fix it!"
-		/>
+			<div className={styles.chatHeads}>
+				<Image
+					className={styles.chatHead01}
+					src="enterprise-offering-v1/7-support/characters/character-1.svg"
+					alt="yo yo"
+				/>
+				<Image
+					src="enterprise-offering-v1/7-support/characters/character-2.svg"
+					alt="how about slack?"
+				/>
+				<Image
+					src="enterprise-offering-v1/7-support/characters/character-3.svg"
+					alt="typing.."
+				/>
+				<Image
+					src="enterprise-offering-v1/7-support/characters/character-4.svg"
+					alt="floating head"
+				/>
+				<Image
+					src="enterprise-offering-v1/7-support/characters/character-5.svg"
+					alt="floating head"
+				/>
+				<Image
+					src="enterprise-offering-v1/7-support/characters/character-6.svg"
+					alt="we can fix it!"
+				/>
+			</div>
+		</div>
 	</div>
 );
--- src/pages/enterprise-offering/sales-cta/sales-cta.tsx
+++ src/pages/enterprise-offering/sales-cta/sales-cta.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 import { Card } from '@bit/bit.base-ui.surfaces.card';
 import { PossibleSizes } from '@bit/bit.base-ui.theme.sizes';
-import { MutedText } from '@bit/bit.base-ui.text.muted-text';
+import { MutedText, mutedText } from '@bit/bit.base-ui.text.muted-text';
 import { TextSeparator } from '@bit/bit.base-ui.text.text-separator';
 import { fullWidth, marginCenter } from '@bit/bit.base-ui.layout.align';
 
@@ -12,47 +12,35 @@ import { H4 } from '../../../elements/heading';
 import { Input, TextArea } from '../../../input/input';
 
 import styles from './sales-cta.module.scss';
+import { margin } from '../spacing';
 
 export const SalesCta = (props: HTMLAttributes<HTMLDivElement>) => (
 	<div {...props} className={classNames(styles.particlesBg, props.className)}>
 		<Card className={classNames(styles.formCard, marginCenter)}>
-			<form
-				onSubmit={(e) => e.preventDefault()}
-				style={{
-					fontSize: '14px',
-					display: 'flex',
-					flexDirection: 'column',
-					height: '100%',
-				}}
-			>
+			<form onSubmit={(e) => e.preventDefault()}>
 				<H4 size={PossibleSizes.xs}>Get in touch</H4>
-				<MutedText style={{ marginBottom: '40px' }}>
+				<MutedText className={margin[30]}>
 					Our expert team will get in touch with you shortly!
 				</MutedText>
-				<Input
-					className={fullWidth}
-					placeholder="Company email"
-					style={{ marginBottom: '18px' }}
-				/>
-				<TextArea
-					className={fullWidth}
-					placeholder="..."
-					style={{ marginBottom: '18px', flexGrow: 1 }}
-				/>
-				<Button importance="cta" className={fullWidth} style={{ marginBottom: '18px' }}>
+
+				<Input className={fullWidth} placeholder="Company email" />
+
+				<TextArea className={fullWidth} placeholder="..." />
+
+				<Button importance="cta" className={fullWidth}>
 					Contact Sales
 				</Button>
 
-				<TextSeparator
-					style={{ marginBottom: '18px', ['--separator-color' as any]: '#ededed' }}
-				>
-					<MutedText>OR</MutedText>
+				<TextSeparator className={classNames(mutedText, styles.separator)}>
+					OR
 				</TextSeparator>
+
 				<Button importance="muted" className={fullWidth}>
 					Book intro
 				</Button>
 			</form>
 		</Card>
+
 		<div className={styles.particlesContainer}>
 			<div className={styles.redParticle} />
 		</div>
