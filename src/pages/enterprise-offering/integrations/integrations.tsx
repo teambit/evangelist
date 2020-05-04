2ca723ddf40cca02d781d9e877b82ccb4293e839
Uri Kutner <urikutner@gmail.com>
2020-05-04 13:45:23 +0300

    ecosystem section - tight icons

--- src/pages/enterprise-offering/integrations/integrations.tsx
+++ src/pages/enterprise-offering/integrations/integrations.tsx
@@ -10,118 +10,118 @@ export function Integrations(props: HTMLAttributes<HTMLDivElement>) {
 		<div className={classNames(props.className, styles.integrationBadges)}>
 			<div className={styles.logos}>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-asana.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/asana.svg"
 					alt="asana"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-jenkins.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/jenkins.svg"
 					alt="jenkins"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-aws.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/aws.svg"
 					alt="aws"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-azure.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/azure.svg"
 					alt="azure"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-bitbucket.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/bitbucket.svg"
 					alt="bitbucket"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-circle.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/circle.svg"
 					alt="circle"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-confluence.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/confluence.svg"
 					alt="confluence"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-github.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/github.svg"
 					alt="github"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-gitlub.svg"
-					alt="gitlub"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/gitlab.svg"
+					alt="gitlab"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-google.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/google-cloud.svg"
 					alt="google"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-jira.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/jira.svg"
 					alt="jira"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-netlify.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/netlify.svg"
 					alt="netlify"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-slack.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/slack.svg"
 					alt="slack"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-tavis.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/tavis.svg"
 					alt="tavis"
 				/>
 
 				{/* SECOND LOOP (for infinite animation) */}
 
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-asana.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/asana.svg"
 					alt="asana"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-jenkins.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/jenkins.svg"
 					alt="jenkins"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-aws.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/aws.svg"
 					alt="aws"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-azure.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/azure.svg"
 					alt="azure"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-bitbucket.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/bitbucket.svg"
 					alt="bitbucket"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-circle.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/circle.svg"
 					alt="circle"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-confluence.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/confluence.svg"
 					alt="confluence"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-github.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/github.svg"
 					alt="github"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-gitlub.svg"
-					alt="gitlub"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/gitlab.svg"
+					alt="gitlab"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-google.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/google.svg"
 					alt="google"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-jira.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/jira.svg"
 					alt="jira"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-netlify.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/netlify.svg"
 					alt="netlify"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-slack.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/slack.svg"
 					alt="slack"
 				/>
 				<Image
-					src="enterprise-offering-v1/5-ecosystem-section/logos/logo-tavis.svg"
+					src="enterprise-offering-v1/5-ecosystem-section/logos/tavis.svg"
 					alt="tavis"
 				/>
 			</div>
