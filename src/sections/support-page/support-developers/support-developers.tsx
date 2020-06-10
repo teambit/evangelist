0cf23e784369504afa96cbd4fa906ea734a63704
oded <oded@bit.dev>
2020-06-10 14:07:22 +0300

    support developers section update

--- src/sections/support-page/support-developers/support-developers.tsx
+++ src/sections/support-page/support-developers/support-developers.tsx
@@ -15,12 +15,10 @@ import styles from './support-developers.module.scss';
 import { Link } from '../../../elements/link';
 import { links } from '../../../content/links';
 
-type SupportDevelopersProps = {
-	
-} & HTMLAttributes<HTMLElement>;
+type SupportDevelopersProps = {} & HTMLAttributes<HTMLElement>;
 
 /**
- * Support section elaborating on the benefits of our support packages 
+ * Support section elaborating on the benefits of our support packages
  * @name SupportDevelopers
  */
 export function SupportDevelopers(props: SupportDevelopersProps) {
@@ -30,37 +28,49 @@ export function SupportDevelopers(props: SupportDevelopersProps) {
 		{
 			iconUrl: 'support-page/tool.svg',
 			title: 'By core maintainers',
-			text:
-				'Get support from the core developers who actually build the tools you work with.',
-			alt: '',
+			text: (
+				<Paragraph>
+					Get support from the core developers who actually build the tools you work
+					with.
+				</Paragraph>
+			),
+			alt: 'Bit - support tools',
 		},
 		{
 			iconUrl: 'support-page/support.svg',
 			title: 'We respond fast',
-			text:
-				'We always do our best to make sure you never have to wait for support when you need it.',
-			alt: '',
+			text: (
+				<Paragraph>
+					We always do our best to make sure you never have to wait for support when
+					you need it.
+				</Paragraph>
+			),
+			alt: 'Bit - support time',
 		},
 		{
 			iconUrl: 'support-page/eye.svg',
 			title: 'We see things through',
-			text:
-				'Work is not done until the problem is solved and everyone are happy with the result.',
-			alt: '',
+			text: (
+				<Paragraph>
+					Work is not done until the problem is solved and everyone are happy with the
+					result.
+				</Paragraph>
+			),
+			alt: 'Bit - support platform',
 		},
 		{
 			iconUrl: 'support-page/book.svg',
 			title: 'With extensive resources',
 			text: (
-				<span>
+				<Paragraph>
 					We constantly write{' '}
 					<Link external href={links.docs}>
 						documentation
 					</Link>
 					, tutorials and guides to support every use case.
-				</span>
+				</Paragraph>
 			),
-			alt: '',
+			alt: 'Bit - support guides',
 		},
 	];
 
@@ -82,10 +92,11 @@ export function SupportDevelopers(props: SupportDevelopersProps) {
 						key={index}
 						iconUrl={card.iconUrl}
 						title={card.title}
-						text={card.text}
 						alt={card.alt}
 						className={classNames(text.center, styles.supportCard)}
-					/>
+					>
+						{card.text}
+					</SupportDevelopersCard>
 				))}
 			</Grid>
 		</div>
@@ -96,23 +107,23 @@ type SupportDevelopersCardProps = {
 	iconUrl: string;
 	alt: string;
 	title?: string;
-	text?: string | ReactNode;
 } & HTMLAttributes<HTMLDivElement>;
 
 export function SupportDevelopersCard({
 	iconUrl,
 	title,
-	text,
 	alt,
 	className,
+	children,
+	...rest
 }: SupportDevelopersCardProps) {
 	return (
-		<div className={classNames(className)}>
+		<div className={classNames(className)} {...rest}>
 			<Image alt={alt} src={iconUrl} className={styles.cardIcon} />
 			<H3 size={PossibleSizes.xs} className={styles.cardTitle}>
 				{title}
 			</H3>
-			<Paragraph>{text}</Paragraph>
+			{children}
 		</div>
 	);
 }
