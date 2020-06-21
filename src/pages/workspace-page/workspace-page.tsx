7ee331d86a9458d7927bd3b62ee7dc5177cbf5b5
oded <oded@bit.dev>
2020-06-21 19:22:49 +0300

    render docs and change log sections

--- src/pages/workspace-page/workspace-page.tsx
+++ src/pages/workspace-page/workspace-page.tsx
@@ -1,8 +1,9 @@
 import React, { HTMLAttributes, ReactNode } from 'react';
 import classNames from 'classnames';
-import { ChangeLog } from '../../sections/workspace-sections/change-log/change-log';
+import { VersionBlock } from '../../sections/workspace-sections/version-block/version-block';
 import styles from './workspace-page.module.scss';
-import {versionExample} from '../workspace-page/change-log.data';
+import { versionsArray } from '../workspace-page/change-log.data';
+import { DocsSection } from '../../sections/workspace-sections/docs-section';
 
 export type WorkSpacePageProps = {} & HTMLAttributes<HTMLDivElement>;
 /**
@@ -12,9 +13,18 @@ export type WorkSpacePageProps = {} & HTMLAttributes<HTMLDivElement>;
 export function WorkSpacePage(props: WorkSpacePageProps) {
 	return (
 		<div>
-			<ChangeLog version={versionExample} className={styles.marginBottom} />
-			<ChangeLog version={versionExample} className={styles.marginBottom} />
-			<ChangeLog version={versionExample} className={styles.marginBottom} />
+			<DocsSection {...docsMock} />
+			{versionsArray.map((version, index) => (
+				<VersionBlock key={index} version={version} className={styles.marginBottom} />
+			))}
+
 		</div>
 	);
+}
+
+
+const docsMock = {
+	title: 'Radio',
+	subTitle: 'Radio Buttons are graphical interface elements that allow user to choose only one of a predefined set of mutually exclusive options.',
+	labels: ['chart', 'graph', 'ui-components', 'react'],
 }
